9f9309eb4248a76d2ebaf5d3e7b0b6870aa60df8
aaaassswwww <23302010020@fudan.edu.cn>
2025-09-27 20:58:24 +0800

    lab1:完成sleep 和 pingpong两个功能

--- user/sleep.c
+++ user/sleep.c
@@ -1,1 +1,14 @@
-please change it!
+#include "user/user.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+      fprintf(2, "sleep: wrong args\n");
+    }else{
+      int time = atoi(argv[0]);
+      sleep(time);
+    }
+    fprintf(1, "(nothing happens for a little while)\n");
+    exit(0);
+}
