fc9feccb0a0890a39e51404603cda62ae218db1c
aaaassswwww <23302010020@fudan.edu.cn>
2025-12-01 12:09:05 +0800

    lab6 finished

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -110,8 +110,9 @@ sys_fstat(void)
   struct file *f;
   uint64 st; // user pointer to struct stat
 
-  if(argfd(0, 0, &f) < 0 || argaddr(1, &st) < 0)
+  if(argfd(0, 0, &f) < 0 || argaddr(1, &st) < 0){
     return -1;
+  }
   return filestat(f, st);
 }
 
@@ -304,11 +305,34 @@ sys_open(void)
       return -1;
     }
   } else {
-    if((ip = namei(path)) == 0){
-      end_op();
-      return -1;
+    int depth = 0;
+    while (1) {
+    
+      if((ip = namei(path)) == 0){
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+
+      if(ip->type == T_SYMLINK && (omode & O_NOFOLLOW) == 0){
+        if(depth++ >= 10){
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        char target[MAXPATH];
+        memset(target, 0, sizeof(target));
+        readi(ip, 0, (uint64)target, 0, MAXPATH);
+        iunlockput(ip);
+        
+        // 使用目标路径继续查找
+        safestrcpy(path, target, MAXPATH);
+        continue;
+      }
+      break;
+
+
     }
-    ilock(ip);
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -482,5 +506,33 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char path[MAXPATH], target[MAXPATH];
+  struct inode *ip;
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+
+  begin_op();
+
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, 0, strlen(target)) != strlen(target)){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
--- user/user.h
+++ user/user.h
@@ -26,6 +26,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int symlink(char*, char*);
 
 // ulib.c
 int stat(const char*, struct stat*);
